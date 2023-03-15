5e36f1a916ccfbb1e2bc04e14b756e3c1c6b4946
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-03-15 15:55:59 +0100

    add App header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script>
-
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'App',
+  components: {AppHeader}
+};
 </script>
 
 <template>
-
   
+  <AppHeader></AppHeader>
+
 </template>
+
+<style>
+/* style */
+</style>
