acc8be7a3b207f42acb953af8d0dd1e6286b7845
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-03-15 16:12:34 +0100

    added axios call and method

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+const url = 'http://127.0.0.1:8000/api/'
 export default {
   name: 'App',
-  components: {AppHeader}
+  components: {AppHeader},
+  data: ()=> ({
+    projects: []
+  }),
+  methods:{
+    fetchProjects(){
+      axios.get(url + 'projects').then((res) =>{
+        this.projects = res.data;
+      })
+    }
+  },
+  created(){
+    this.fetchProjects();
+  }
 };
 </script>
 
 <template>
   
   <AppHeader></AppHeader>
-
+  <main class="container">
+    <ul>
+      <li v-for="project in projects">{{ project.title }}</li>
+    </ul>
+  </main>
 </template>
 
 <style>
